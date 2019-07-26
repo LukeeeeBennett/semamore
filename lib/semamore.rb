1decb56d3a8a4e736c9ab2ca7f729225fc0c53e8
Luke Bennett <lukeeeebennettplus@gmail.com>
2019-07-26 09:22:43 +0100

    Add verbosity

--- lib/semamore.rb
+++ lib/semamore.rb
@@ -4,15 +4,19 @@ require 'semamore_octo_client'
 
 class Semamore
   def initialize(token:, client_options: {})
+    p 'Starting semamore...'
     @client = ::SemamoreOctoClient.new(token, options: client_options)
   end
 
   def changed_files_guard(repo:, branch:, command:, pattern:)
+    p "Finding changed files on #{branch} that match #{pattern}..."
     files = client.current_pr_files(repo, branch)
 
     if has_matching_files?(pattern, files)
+      p "Found matching changed files, executing `#{command}`..."
       exec command
     else
+      p "No matching changed files found, skipping `#{command}`."
       exit 0
     end
   end
--- lib/semamore_octo_client.rb
+++ lib/semamore_octo_client.rb
@@ -6,14 +6,17 @@ class SemamoreOctoClient
   end
 
   def current_pr_files(repo, branch)
+    p 'Fetching current PR changed files...'
     client.pull_request_files(repo, current_pr(repo, branch)[:number])
   end
 
   def current_pr(repo, branch)
+    p 'Selecting current PR...'
     open_prs(repo).select {|pr| pr[:head][:ref] == branch}.first
   end
 
   def open_prs(repo)
+    p 'Fetching open PRs...'
     client.pull_requests(repo, :state => "open")
   end
 
